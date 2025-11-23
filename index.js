283b170593c25e6568a53b67055a634f4dfc599f
wendeldealmeida2015-ai <wendeldealmeida2015@gmail.com>
2025-11-22 21:43:53 -0300

    Update import path for paymentRoutes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import paymentRoutes from './src/routes/paymentRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
 import fs from 'fs';
 import path from 'path';
 
